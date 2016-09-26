b51de84f2fdf6eb94330e6b2eb0fecba2e11e1f1
McFeod <macfeod@yandex.ru>
2016-09-26 16:33:33 +0400

    fixed lf line-endings

--- LabOneCinema/Program.cs
+++ LabOneCinema/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using LabOneCinema.Artifacts;
 using LabOneCinema.Collections;
-using LabOneCinema.Delegates;
 using LabOneCinema.Factory;
 using LabOneCinema.Logging;
 
